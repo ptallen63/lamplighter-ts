a629be290ea2a19581f293aa395b75c9273b0a5f
Paul Allen <ptallen63@gmail.com>
2019-12-20 10:05:14 -0500

    optomizing webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,45 @@
 const webpack = require("webpack");
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const pkg = require('./package.json');
 
 const plugins = [
+  new CleanWebpackPlugin(),
   new HtmlWebpackPlugin({
     template: './src/index.html'
   }),
   new webpack.BannerPlugin(`
   Lamplighter.js
   v${pkg.version}
-  Documentation: https://github.com/HigherEducation/fusion
+  Documentation: https://github.com/ptallen63/lamplighter
 `),
 ];
 
 module.exports = {
 
   // webpack will take the files from ./src/index
-  entry: './src/index',
+  entry: {
+   "lamplighter" : './src/index',
+  "lampligher.min": './src/index',
+  },
+  devtool: 'cheap-source-map',
 
   // and output it into /dist as bundle.js
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: 'fusion.js'
+    filename: '[name].js',
+    library: 'lamplighter',
+    libraryTarget: 'umd',
+  },
+
+  optimization: {
+    minimize: true,
+    minimizer: [new UglifyJsPlugin({
+      include: /\.min\.js$/,
+      sourceMap: true,
+    })]
   },
 
   // adding .ts and .tsx to resolve.extensions will help babel look for .ts and .tsx files to transpile
